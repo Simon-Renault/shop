0a1972f24afd12e5061cf88577dae5174586e069
simon <simon.renault.pro@gmail.com>
2020-09-30 08:55:12 +0100

    fixed template bug

---
+++ src/pages/Blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+
+
+    <v-centered-container>
+
+    </v-centered-container>
+   
+  </Layout>
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<script>
+
+</script>
